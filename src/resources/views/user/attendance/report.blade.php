fc8cca3339166af6be2f7533e42a4e596677ba8e
Maki <mtakagi0526@yahoo.co.jp>
2026-08-11 23:33:18 +0900

    update

--- src/resources/views/user/attendance/report.blade.php
+++ src/resources/views/user/attendance/report.blade.php
@@ -3,50 +3,58 @@
 @section('content')
 <link href="{{ asset('css/user/report.css') }}" rel="stylesheet">
 
+<main class="report-page">
+
 <div class="container">
     <h3 class="title">マイ勤怠レポート</h3>
     <h4 class="text-a">過去6ヶ月の勤怠データから集計しています。</h4>
     <a class="text-a">基本サマリー</a>
 
-    <div class="summery">
-    <p>
-        総労働時間
-        {{ intdiv($totalWorkMinutes, 60) }}h
-        {{ $totalWorkMinutes % 60 }}m
-    </p>
+    <div class="summary">
+        <div class="summary-box">
+            <p class="box-title">総労働時間</p>
+            <span class="content">
+                {{ intdiv($totalWorkMinutes, 60) }}h
+                {{ $totalWorkMinutes % 60 }}m
+            </span>
+        </div>
 
-    <p>
-        総残業時間
-        {{ intdiv($totalOvertimeMinutes, 60) }}h
-        {{ $totalOvertimeMinutes % 60 }}m
-    </p>
+        <div class="summary-box">
+            <p class="box-title">総残業時間</p>
+            <span class="content">
+                {{ intdiv($totalOvertimeMinutes, 60) }}h
+                {{ $totalOvertimeMinutes % 60 }}m
+            </span>
+        </div>
 
-    <p>
-        平均労働時間/日
-        {{ intdiv($averageWorkMinutes, 60) }}h
-        {{ $averageWorkMinutes % 60 }}m
-    </p>
+        <div class="summary-box">
+            <p class="box-title">平均労働時間/日</p>
+            <span class="content">
+                {{ intdiv($averageWorkMinutes, 60) }}h
+                {{ $averageWorkMinutes % 60 }}m
+            </span>
+        </div>
     </div>
 
     <a class="text-a">月次推移 (過去6ヶ月)</a>
 
     <table class="m-table">
         <tr>
-            <th>月</th>
-            <th>労働時間</th>
-            <th>残業時間</th>
+            <th class="month">月</th>
+            <th class="work-time">労働時間</th>
+            <th class="overtime">残業時間</th>
         </tr>
 
         @foreach($monthlyReports as $report)
             <tr>
-                <td>{{ $report['month'] }}</td>
+                <td class="month">{{ $report['month'] }}</td>
 
-                <td>
+                <td class="work-time">
                     {{ intdiv($report['work_minutes'], 60) }}h
                     {{ $report['work_minutes'] % 60 }}m
                 </td>
 
-                <td>
+                <td class="overtime">
                     {{ intdiv($report['overtime_minutes'], 60) }}h
                     {{ $report['overtime_minutes'] % 60 }}m
                 </td>
@@ -55,18 +63,26 @@
     </table>
 
     <h4 class="text-a">異常検知</h4>
-    <a class="text-a">基準：始業09:00/終業 18:00/長時間労働は一日10時間超
+    <p class="box-title">基準：始業09:00/終業 18:00/長時間労働は一日10時間超</p>
 
-    <div class="summery">
-            <p>遅刻回数
-            {{ $lateCount }}回</p>
+    <div class="summary">
+        <div class="summary-box">
+            <p class="box-title">遅刻回数</p>
+            <span class="content">{{ $lateCount }}回</span>
+        </div>
 
-            <p>早退回数
-            {{ $earlyLeaveCount }}回</p>
+        <div class="summary-box">
+            <p class="box-title">早退回数</p>
+            <span class="content">{{ $earlyLeaveCount }}回</span>
+        </div>
 
-            <p>長時間労働回数
-            {{ $longWorkCount }}回</p>
+        <div class="summary-box">
+            <p class="box-title">長時間労働回数</p>
+            <span class="content">{{ $longWorkCount }}回</span>
+        </div>
     </div>
-
 </div>
+
+</main>
+
 @endsection
